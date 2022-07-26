e5ae12ccad2f6f69503be60fef8481e0f384aa90
WilMartins258 <willmartins258@gmail.com>
2022-07-26 10:16:37 -0300

    feat: documentação das funções-ajuste de variáveis

--- willian/willian-exercicio1/js/script.js
+++ willian/willian-exercicio1/js/script.js
@@ -1,48 +1,65 @@
-/** WILLIAN MARTINS
- *  Decidi fazer uma função separada para cada conversão, mais ou menos do meio para o fim do projeto eu notei que não era uma ideia boa
- * 
+/** 
+ *  WILLIAN MARTINS
+ * Conversão de valores em diferentes unidades de medidas.
  */
 
+/**
+ * Função que converte metro em centímetro
+ */
 function convertMetroCentimetro() {
-    let numMetr = document.getElementById('inputMetro')
-    let lblCent = document.getElementById('lblCent')
-    let numCent = Number(numMetr.value) * 100
+    const numMetr = document.getElementById('inputMetro')
+    const lblCent = document.getElementById('lblCent')
+    const numCent = Number(numMetr.value) * 100
     lblCent.innerHTML = (`<strong>${numCent.toFixed(3).replace('.', ',')}cm</strong>`)
 }
 
+/**
+ * Função que converte centímetro em metro
+ */
 function convertCentimetroMetro() {
-    let numCent = document.getElementById('inputCent')
-    let lblMet = document.getElementById('lblMet')
-    let numMet= Number(numCent.value) / 100
+    const numCent = document.getElementById('inputCent')
+    const lblMet = document.getElementById('lblMet')
+    const numMet= Number(numCent.value) / 100
     lblMet.innerHTML = (`<strong>${numMet.toFixed(3).replace('.', ',')}m</strong>`)
 }
 
+/**
+ * Função que converte graus farenheit em graus celcius
+ */
 function convertFarenheitCelcius() {
-    let numFar = document.getElementById('inputFaren')
-    let lblCel = document.getElementById('lblCel')
-    let numCel= (Number(numFar.value) - 32) / 1.8
+    const numFar = document.getElementById('inputFaren')
+    const lblCel = document.getElementById('lblCel')
+    const numCel= (Number(numFar.value) - 32) / 1.8
     lblCel.innerHTML = (`<strong>${numCel.toFixed(3).replace('.', ',')}°C</strong>`)
 } 
 
-
+/**
+ * Função que converte graus celcius em graus farenheit
+ */
 function convertCelciusFarenheit() {
-    let numCel = document.getElementById('inputCelcius')
-    let lblFar = document.getElementById('lblFar')
-    let numFar= (1.8 * Number(numCel.value)) + 32 
+    const numCel = document.getElementById('inputCelcius')
+    const lblFar = document.getElementById('lblFar')
+    const numFar= (1.8 * Number(numCel.value)) + 32 
     lblFar.innerHTML = (`<strong>${numFar.toFixed(3).replace('.', ',')}°F</strong>`)
 }
 
+/**
+ * Função que converte reais em dólares
+ */
 function convertRealDolar() {
-    let numReal = document.getElementById('inputReal')
-    let lblDol = document.getElementById('lblDol')
-    let numDol=  Number(numReal.value) / 5.5
+    const numReal = document.getElementById('inputReal')
+    const lblDol = document.getElementById('lblDol')
+    const numDol=  Number(numReal.value) / 5.5
     lblDol.innerHTML = (`<strong>US$${numDol.toFixed(2)}</strong>`)
 }
 
+/**
+ * Função que converte dólares em reais
+ */
 function convertDolarReal() {
-    let numDol = document.getElementById('inputDolar')
-    let lblReal = document.getElementById('lblReal')
-    let numReal=  Number(numDol.value) * 5.5
+    const numDol = document.getElementById('inputDolar')
+    const lblReal = document.getElementById('lblReal')
+    const numReal=  Number(numDol.value) * 5.5
     lblReal.innerHTML = (`<strong>R$${numReal.toFixed(2).replace('.', ',')}</strong>`)
 }
 
